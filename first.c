9e27e8ca1e7756c4244bdc9665a4cd2118f7c9f6
Sudhagar_vetrivel <59435694+sudha2327@users.noreply.github.com>
2022-11-02 22:35:12 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,15 @@
+#include<stdio.h>    //header file 
+int main(void){     //main () function
+	   
+	  // printf("helloworld");
+	  int a=10,b=30;                    //init
+       int sub=0; 
+	
+	  sub=b-a;
+	  printf("%d",sub);
+	  
+	   return 0;
+	  
+}
+
+
